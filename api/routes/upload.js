a61eca5964b1a6f44164fc7b0c1f79a182e34569
磨痕 <251205668@qq.com>
2021-04-10 23:15:21 +0800

    format🥚 格式化代码

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -4,22 +4,21 @@ const router = new Router()
 const UploadController = require('../../controller/upload')
 
 // 上传单文件
-router.post('/upload',UploadController.uploadFile)
+router.post('/upload', UploadController.uploadFile)
 
 // 上传多文件
-router.post('/uploads',UploadController.uploadFiles)
+router.post('/uploads', UploadController.uploadFiles)
 
 // 删除一个或者多个文件
-router.delete('/upload',UploadController.removeFile)
+router.delete('/upload', UploadController.removeFile)
 
 // 获取指定id文件信息
-router.get('/file',UploadController.findFileById)
+router.get('/file', UploadController.findFileById)
 
 // 获取当前用户上传文件列表
-router.get('/uploads',UploadController.getFileListByUser)
+router.get('/uploads', UploadController.getFileListByUser)
 
 // 获取系统文件列表
-router.get('/files',UploadController.getFileList)
+router.get('/files', UploadController.getFileList)
 
 module.exports = router
-
